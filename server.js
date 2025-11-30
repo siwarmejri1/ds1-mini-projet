c02552850fa0a2561b35ef223a172e353c1b13b4
Fatmaderouiche <derouicheftm@gmail.com>
2025-11-30 15:37:11 +0100

    fix: modification dans server.js

--- server.js
+++ server.js
@@ -4,19 +4,23 @@ const connectDB = require('./config/db');
 const projectRoutes = require('./src/routes/projectRoutes');
 const taskRoutes = require('./src/routes/taskRoutes');
 
+//hnee aamlna initialisation mtaa l app express
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Connexion à MongoDB
+//  l connexion mtaa l base de données 
 connectDB();
-
-// Middleware
 app.use(express.json());
 
-// route
+
+// route te3 projects w tasks
 app.use('/projects', projectRoutes);
 app.use('/tasks', taskRoutes);
 
 
-// Démarrer le serveur
-app.listen(port, () => console.log(`Serveur démarré sur le port ${port}`));
+
+//besh ndemarriou l app
+app.listen(port, () => {
+  console.log(` Serveur démarré sur le port ${port}`);
+   console.log(` URL: http://localhost:${port}`);
+});
