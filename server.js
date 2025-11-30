75e88f751b637ba0678e2d3717d4584ed10bff33
Fatmaderouiche <derouicheftm@gmail.com>
2025-11-30 15:37:11 +0100

    fix: modification dans server.js

--- server.js
+++ server.js
@@ -3,20 +3,37 @@ const express = require('express');
 const connectDB = require('./config/db');
 const projectRoutes = require('./src/routes/projectRoutes');
 const taskRoutes = require('./src/routes/taskRoutes');
-
+const userRoutes = require('./src/routes/users');
+//hnee aamlna initialisation mtaa l app express
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Connexion à MongoDB
+//  l connexion mtaa l base de données 
 connectDB();
-
-// Middleware
 app.use(express.json());
+//route besh ntesstiou byh 
+app.get('/', (req, res) => {
+  res.json({ 
+    message: ' API User Authentication is working!',
+    status: 'Server is running',
+    database: 'MongoDB connecting...',
+    endpoints: {
+      register: 'POST /api/users/register',
+      login: 'POST /api/users/login', 
+      profile: 'GET /api/users/profile'
+    }
+  });
+});
+// l route mtaa l api 
+app.use('/api/users', userRoutes);
 
-// route
+// route te3 projects w tasks
 app.use('/projects', projectRoutes);
 app.use('/tasks', taskRoutes);
 
 
-// Démarrer le serveur
-app.listen(port, () => console.log(`Serveur démarré sur le port ${port}`));
+//besh ndemarriou l app
+app.listen(port, () => {
+  console.log(` Serveur démarré sur le port ${port}`);
+   console.log(` URL: http://localhost:${port}`);
+});
