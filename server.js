eac1813c66f45e24596379887ae527e5c39c948f
Mejri Siwar <mejrisiwar473@gmail.com>
2025-11-30 12:12:11 +0100

    config: serveur Express avec middleware JSON et connexion MongoDB

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const userRoutes = require('./src/routes/users');
+//hnee aamlna initialisation mtaa l app express
+const app = express();
+const port = process.env.PORT || 5000;
+
+// l connexion mtaa l base de données 
+connectDB();
+
+
+app.use(express.json());
+//route besh ntesstiou byh 
+app.get('/', (req, res) => {
+  res.json({ 
+    message: ' API User Authentication is working!',
+    status: 'Server is running',
+    database: 'MongoDB connecting...',
+    endpoints: {
+      register: 'POST /api/users/register',
+      login: 'POST /api/users/login', 
+      profile: 'GET /api/users/profile'
+    }
+  });
+});
+// l route mtaa l api 
+app.use('/api/users', userRoutes);
+
+//besh ndemarriou l app
+app.listen(port, () => {
+  console.log(` Serveur démarré sur le port ${port}`);
+   console.log(` URL: http://localhost:${port}`);
+});
