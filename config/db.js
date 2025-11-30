701e9ad27348146b43bb72878a04a49a9a407e96
Mejri Siwar <mejrisiwar473@gmail.com>
2025-11-30 12:16:39 +0100

    fix: db.js - suppression options MongoDB obsolètes

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 //naamlou fonction besh nconnectiou bel base 
 const connectDB = async () => { 
     try {
-        await mongoose.connect(process.env.MONGO_URI, {  
-            useNewUrlParser: true, 
-            useUnifiedTopology: true, 
-        });
-        console.log("MongoDB connecté avec succès"); // ken connectina ça va l message hedha yodhher fel console
+        const conn = await mongoose.connect(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/ds1-mini-projet'
+    );
+     console.log(` MongoDB connecté: ${conn.connection.host}`);
+     console.log(` Base de données: ${conn.connection.name}`);
+     console.log("MongoDB connecté avec succès");  // ken connectina ça va l message hedha yodhher fel console
     } catch (error) {
         console.error("Erreur de connexion MongoDB :", error.message); // ken fama erreur lmessage hedha yodhher fel console 
         process.exit(1); // hedha ywaqef l applicqation ken fama erreur 
