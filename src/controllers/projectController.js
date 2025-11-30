2b9034e8100a2b1c066aef3c3a9ab0837fed6190
Fatmaderouiche <derouicheftm@gmail.com>
2025-11-30 11:33:43 +0100

    feat: ajout  de la fonctionnalité tri et recherche d un projet et mmodification des routes

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -47,3 +47,26 @@ exports.allProjects = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+// tri+recherche fel les projets
+exports.searchtriProjects = async (req, res) => {
+  const { sort, order = "asc", search } = req.query;
+
+  let query = req.user.role === "manager" ? {} : { proprietaire: req.user._id };
+
+  // Recherche par nom ?search=aaa
+  if (search) {
+    query.nom = { $regex: search, $options: "i" };
+  }
+
+  // Tri par champ (nom ,datecreation...) + par odre croissant/decroissant
+  //  ?sort=nom&order=asc
+  let sortOption = {};
+  if (sort) {
+    sortOption[sort] = order === "desc" ? -1 : 1;
+  }
+
+  const projects = await Project.find(query).sort(sortOption);
+
+  res.json(projects);
+};
--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -4,16 +4,18 @@ const router = express.Router();
 const { 
   createProject,
     myProjects,
-    allProjects
+    allProjects,
+    searchtriProjects
 } = require('../controllers/projectController');
 
 const { authenticate, isManager } = require('../middleware/auth');
 
 
-// route mte3 creation projet
+// route mte3 les projet ( creation, liste, tri+recherche )
 router.post('/create', authenticate, createProject);
 router.get('/mine', authenticate, myProjects);
 router.get('/all', authenticate, isManager, allProjects);
+router.get('/search',authenticate, searchtriProjects);
 
 
 
